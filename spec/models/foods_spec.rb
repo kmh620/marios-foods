83b5b4fed16db192481852ce3e70ce20033b2f5b
Katrina Hockman <khockman90@hotmail.com>
2018-12-14 15:49:00 -0800

    Attempt to fix rspec errors, didnt pass

--- spec/models/foods_spec.rb
+++ spec/models/foods_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 describe Food do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :origin }
   it { should have_many :reviews }
 end
