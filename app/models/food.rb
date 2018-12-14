d2768aafb0490ad413fbd6ae9b160f4a42c65a73
Katrina Hockman <khockman90@hotmail.com>
2018-12-14 12:58:52 -0800

    Add begining use of scopes, not shown in html

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,13 @@ class Food < ActiveRecord::Base
   validates :name, :presence => true
   validates :price, :presence => true
   validates :origin, :presence => true
+
+  scope :three_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_reviews, -> {(
+    select("foods.id, foods.name, foods.price, foods.origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("foods.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
 end
