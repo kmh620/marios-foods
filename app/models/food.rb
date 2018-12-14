e2ead7338f92213ff778f923379d29b903d83610
Katrina Hockman <khockman90@hotmail.com>
2018-12-14 09:43:40 -0800

    Add food show page- no content

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ActiveRecord::Base
+  has_many :reviews
   validates :name, :presence => true
 end
--- db/migrate/20181214163150_create_foods.rb
+++ db/migrate/20181214163150_create_foods.rb
@@ -2,6 +2,8 @@ class CreateFoods < ActiveRecord::Migration[5.2]
   def change
     create_table :foods do |t|
       t.column :name, :string
+      t.column :price, :integer
+      t.column :origin, :string
 
       t.timestamps
     end
